3c8f343a9cf0a5a9966425e62932f036f445c3f6
Štěpán Kovačič <stepan.kovacic@gmail.com>
2023-09-01 16:39:25 +0200

    tweaked hello.py

--- hello.py
+++ hello.py
@@ -30,7 +30,7 @@ def simon():
 def page_not_found(e):
     return render_template("404.html"), 404
 
-#internal server error
+#internal server error stufff
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
